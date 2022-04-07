450e2f295a444df301f6780a3d1cc3e7d218e2dd
quaydragon <quaydragon@Quays-MacBook-Pro.local>
2022-04-07 11:10:49 -0400

    Basic Autoencoder made: Testing Time

--- model/autoencoder.py
+++ model/autoencoder.py
@@ -11,24 +11,17 @@ nn = torch.nn
 
 class AutoEncoder(nn.Module):
 
-    def __init__(self, num_features, hidden,  out_dim):
+    def __init__(self, num_features, hidden,  out_dim, dropout):
         self.encoder = []
         self.decoder = []
         self.num_features = num_features
         self.out_dim = out_dim
         self.hidden = hidden
         self.buildEncoderDecoder()
+        self.dropout = dropout
 
     def buildEncoderDecoder(self):
 
-        ## gaussian Noise can be placed
-
-        ## TODO: Normalize in the forward function
-
-        ## TODO: Add Gaussian Noise
-        ## Please Note that the Gaussian Noise can be added in the forward
-
-        ## Dense is equivalent
         self.encoder += [nn.Linear(self.num_features, self.hidden)]
         self.encoder += [nn.BatchNorm1d(self.hidden)]
         self.encoder += [nn.SiLU()]
@@ -37,17 +30,36 @@ class AutoEncoder(nn.Module):
         self.decoder += [nn.Linear(self.hidden, self.hidden)]
         self.decoder += [nn.BatchNorm1d(self.hidden)]
         self.decoder += [nn.SilU()]
-
-        self.decoder = [nn.Linear(self.hidden, self.out_dim)]
+        self.decoder += [nn.Dropout(self.dropout)]
+        self.decoder += [nn.Linear(self.hidden, self.out_dim)]
         ## TODO: DropOut to be added in the forward
 
 
     def forward(self, x):
 
-        ##TODO: 1. Batch Norm
+
+
+        auto = nn.BatchNorm1d(x)
+
         ## TODO 2: Gaussian Noise
 
+
+        ## Noise example Not sure if I will use
+
+        sampled_noise = self.noise.repeat(*x.size()).normal_()
+        auto = auto + sampled_noise
+
+        encode = nn.Sequential(*self.encoder)
+
+        auto = encode(auto)
+
+        decode = nn.Sequential(*self.decoder)
+
+        out = decode(auto)
+
+
         ##TODO: 3: Prepare Input for number of hidden
+        ## This process will be done after we have the regular encoder - decoder working
 
         # x = tf.keras.layers.Concatenate()([x0, encoder])
         # x = tf.keras.layers.BatchNormalization()(x)
@@ -57,7 +69,7 @@ class AutoEncoder(nn.Module):
 
 
 
-        pass 
+        return out
 
     #
 
