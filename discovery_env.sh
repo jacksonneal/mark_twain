7ff3ee43465c82ebfb83776266e75200c2090c8a
Jackson Neal <jacksontneal@gmail.com>
2022-04-07 21:57:31 -0400

    fix readme

--- discovery_env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-module load anaconda3/2022.01 &&
-module load cuda/11.1 &&
-source activate pytorch_env_training
--- discovery_gpu.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-srun --partition=gpu --nodes=1 --pty --gres=gpu:v100-sxm2:1 --ntasks=1 --mem=4GB --time=08:00:00 /bin/bash
