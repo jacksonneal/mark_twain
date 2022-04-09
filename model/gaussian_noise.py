d63d0a91b60f3a2d4d8e9bca475b6368e7db9a67
Jackson Neal <jacksontneal@gmail.com>
2022-04-09 13:12:18 -0400

    try no detach

--- model/gaussian_noise.py
+++ model/gaussian_noise.py
@@ -30,8 +30,8 @@ class GaussianNoise(LightningModule, ABC):
         self.noise = torch.tensor(0, device=self.device)
 
     def forward(self, x):
-        if self.training and self.sigma != 0:
-            scale = self.sigma * x.detach() if self.is_relative_detach else self.sigma * x
-            sampled_noise = self.noise.repeat(*x.size()).float().normal_() * scale
-            x = x + sampled_noise
+        # if self.training and self.sigma != 0:
+        #     scale = self.sigma * x.detach() if self.is_relative_detach else self.sigma * x
+        #     sampled_noise = self.noise.repeat(*x.size()).float().normal_() * scale
+        #     x = x + sampled_noise
         return x
