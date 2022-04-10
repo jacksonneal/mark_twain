7a2dd60058af1c4dd21dec81e97517f8168677df
quaydragon <quaydragon@Quays-MacBook-Pro.local>
2022-04-10 17:57:49 -0400

    Trying different autoencoder

--- model/model_factory.py
+++ model/model_factory.py
@@ -1,6 +1,7 @@
-from model.ae import AE
+# from model.ae import AE
 from model.base import Base
 from model.autoencoder import AutoEncoder
+from model.ae_test import AE
 
 
 def build_model(params):
@@ -9,6 +10,6 @@ def build_model(params):
     elif params.model_name == "AE":
         return AE(params)
     elif params.model_name == "AUTO":
-        return AutoEncoder(params)
+        return AE(params)
     else:
         raise Exception(f"unsupported model name {params.model_name}")
