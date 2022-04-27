a3291ff0c8a64934ecd02c954bf193f444af2da2
quaydragon <quaydragon@Quays-MacBook-Pro.local>
2022-04-27 14:21:15 -0400

    Setting up models

---
+++ model/AE_CONV.py
@@ -0,0 +1,40 @@
+from abc import ABC
+
+import torch
+from pytorch_lightning import LightningModule
+from torch import nn
+from model.autoencoder import AutoEncoder
+from model.ae import AE
+nn = torch.nn
+
+
+class AEConv(LightningModule, ABC):
+    def __init__(self, params):
+        super().__init__()
+        print(params)
+        self.num_features = params.dimensions[0]
+        # self.ae = AutoEncoder(params)
+        self.ae = AE(params)
+        self.params = params
+
+        """
+        
+        Here is the deal I am not sure how to change the dimensions on this one. 
+        It would be great if I could change the yaml files but I am struggling with that. 
+        
+        
+        
+        """
+
+    def forward(self, x):
+
+        # batch1 = nn.BatchNorm1d(self.num_features)
+        out = self.ae(x)
+
+        out = 0
+
+        return out
+
+
+
+
