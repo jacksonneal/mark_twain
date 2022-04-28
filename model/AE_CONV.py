7c07c5bb06526355b79668575c9a54845d2c70f2
quaydragon <quaydragon@Quays-MacBook-Pro.local>
2022-04-28 15:56:09 -0400

    Created a model with upscaling

--- model/AE_CONV.py
+++ model/AE_CONV.py
@@ -110,10 +110,10 @@ class AEConv(LightningModule, ABC):
 
 
         x = self.max_pool1(x)
-        x = nn.Relu(x)
+        x = nn.ReLU(x)
         x = self.conv2(x)
         x = self.max_pool2(x)
-        x = nn.Relu(x)
+        x = nn.ReLU(x)
         x = self.conv3(x)
         x = self.max_pool3(x)
 
@@ -121,20 +121,20 @@ class AEConv(LightningModule, ABC):
         x = self.convmid1(x)
         x = self.convmid2(x)
         x = self.convmid3(x)
-        x = nn.Relu(x)
+        x = nn.ReLU(x)
 
 
 
         x = x.squeeze()
 
         x = self.linear1(x)
-        x = nn.Relu(x)
+        x = nn.ReLU(x)
 
         x = x.unsqueeze(dim=2)
         x = self.mid1(x)
         x = self.mid2(x)
         x = self.mid3(x)
-        x = nn.Relu(x)
+        x = nn.x(x)
 
         # Decoding
 
