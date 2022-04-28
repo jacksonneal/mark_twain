d5ef435ff97d846d4515c4220f32ce34d0388802
quaydragon <quaydragon@Quays-MacBook-Pro.local>
2022-04-28 07:51:18 -0400

    Using large epoch for fun

--- model/AE_CONV.py
+++ model/AE_CONV.py
@@ -23,6 +23,8 @@ class AEConv(LightningModule, ABC):
         The idea is to go around the dimensions
         The dimensions should dip down in the middle and then output 1
         """
+        self.kernel = params.kernel
+        ## may be unneccesary
         def dimension_calc(input, kernel, stride=1, padding=0, dilation=1):
             shape = input.shape
             last_dim = shape[-1]
@@ -36,12 +38,12 @@ class AEConv(LightningModule, ABC):
         self.num_feats = self.dimensions[0]
         self.encoder = []
         # First down
-        self.conv1 = nn.Conv1d(self.num_feats, 40, 1)
+        self.conv1 = nn.Conv1d(self.num_feats, 40, self.kernel)
 
         self.max_pool1 = nn.MaxPool1d(1, stride=1)
 
         # Second Down
-        self.conv2 = nn.Conv1d(40, 20, 1)
+        self.conv2 = nn.Conv1d(40, 20, self.kernel)
         self.max_pool2 = nn.MaxPool1d(1, stride=1)
 
         # Third Down
