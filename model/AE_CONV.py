c1f4f95ff6c27550006b9f8052926a7dce1348a4
quaydragon <quaydragon@Quays-MacBook-Pro.local>
2022-04-29 10:34:02 -0400

    Drop out after conv3. no other

--- model/AE_CONV.py
+++ model/AE_CONV.py
@@ -121,7 +121,7 @@ class AEConv(LightningModule, ABC):
         x = self.conv2(x)
         x = self.batch_norm2(x)
         x = self.silu(x)
-        x = self.dropout(x)
+        # x = self.dropout(x)
         x = self.max_pool2(x)
         # x = m(x)
         x = self.conv3(x)
@@ -129,6 +129,7 @@ class AEConv(LightningModule, ABC):
         # # this actually reduced the correlation
         # x = self.batch_norm3(x)
         # x = self.silu(x)
+        x = self.dropout(x)
         x = self.max_pool3(x)
 
         # middle Section
